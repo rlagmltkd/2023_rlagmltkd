289437115efbb154c560e43ffcad05acce1d355c
User <huisang1016@gmail.com>
2023-09-23 09:43:19 +0900

    while
    Signed-off-by: User <huisang1016@gmail.com>

--- whileStatement.py
+++ whileStatement.py
@@ -1,25 +1,50 @@
-number = 0
+# number = 0
+#
+# while number < 5:
+#     print(number)
+#     number = number + 1
+#
+#     print("=====메뉴=====")
+#     print("1. 시작하기")
+#     print("2. 종료하기")
+#     print("==============")
+#
+#     user_input = -1
+#
+#     while user_input != 2:
+#         user_input = int(input("값을 입력하세요 >>"))
+#
+# while True:
+#     print("=====메뉴=====")
+#     print("1. 시작하기")
+#     print("2. 종료하기")
+#     print("==============")
 
-while number < 5:
-    print(number)
-    number = number + 1
+# user_input = int(input("값을 입력하세요 >>"))
+# if user_input == 2:
+#     break
+count = 3
+import random
 
-    print("=====메뉴=====")
-    print("1. 시작하기")
-    print("2. 종료하기")
-    print("==============")
+answer = random.randrange(0, 10)
+user_input = -1
+# 사용자가 answer 맞출 때까지 반복
+# 1. 사용자에게 기회주기(3번)
+# 2. 틀렸을 때 updown 출력해주기
 
-    user_input = -1
-
-    while user_input != 2:
-        user_input = int(input("값을 입력하세요 >>"))
-
-while True:
-    print("=====메뉴=====")
-    print("1. 시작하기")
-    print("2. 종료하기")
-    print("==============")
-
+while user_input != answer:
     user_input = int(input("값을 입력하세요 >>"))
-    if user_input == 2:
+
+    if user_input == answer:
+        print("정답입니다!")
         break
+    elif user_input > answer:
+        print("down")
+        count == count - 1
+
+    elif user_input < answer:
+        print("up")
+        count == count - 1
+
+if count == 0:
+    print("game over")
